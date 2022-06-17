21263eb6800fcca4c9a39270451c56920879042e
AntonioTadeu <49735546+AntonioTadeu@users.noreply.github.com>
2022-06-17 02:07:14 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+
+    public String elogiarSecretaria(String mensagem) {
+        return Ouvidoria.getInstancia().receberElogioSecretaria(mensagem);
+    }
+
+    public String reclamarSecretaria(String mensagem) {
+        return Ouvidoria.getInstancia().receberReclamacaoSecretaria(mensagem);
+    }
+
+    public String sugerirSecretaria(String mensagem) {
+        return Ouvidoria.getInstancia().receberSugestaoSecretaria(mensagem);
+    }
+
+
+}
